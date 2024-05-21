a5022b94a2dae2035edabcb4b22a3fa67b1dd374
liji <896126514@qq.com>
2024-05-21 22:11:40 +0800

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,3 @@
-/**
- * Standalone Express Application
- * for vercel serverless functions
- */
-
-import express from 'express'
-import type { Express } from 'express'
-import helloApp from './hello'
-
-const app: Express = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helloApp)
-export default app
+export function GET(request: Request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`)
+}
