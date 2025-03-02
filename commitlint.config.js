2cc04422c7f32b6b374d3073b98b06256977e18f
liji <896126514@qq.com>
2025-03-02 12:41:39 +0800

    chore: configure commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -50,6 +50,10 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', Object.keys(typeEnum)],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0],
   },
   prompt: {
     messages: {
